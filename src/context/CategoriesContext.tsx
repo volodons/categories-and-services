2ba182b4cd5ec5944e87e6b54914b74b4898350c
Volodymyr Donets <volodons@gmail.com>
2023-10-05 17:52:27 +0300

    Move adding and removing categories functionality into Categories Context

--- src/context/CategoriesContext.tsx
+++ src/context/CategoriesContext.tsx
@@ -5,9 +5,20 @@ const CategoriesContext = createContext();
 export const CategoriesProvider = ({ children }) => {
   const [categories, setCategories] = useState([]);
 
+  const addCategory = (category) => {
+    setCategories([...categories, category]);
+  };
+
+  const removeCategory = (categoryId) => {
+    const updatedCategories = categories.filter(
+      (category) => category.id !== categoryId
+    );
+    setCategories(updatedCategories);
+  };
+
   const categoriesContextValue = useMemo(
-    () => ({ categories, setCategories }),
-    [categories, setCategories]
+    () => ({ categories, setCategories, addCategory, removeCategory }),
+    [categories, setCategories, addCategory, removeCategory]
   );
 
   return (
